1d8af286a6bf890fe7b8e3cad1685c0ae15e01b0
P <prasen@rathore.com.au>
2016-07-02 03:06:47 +0930

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,9 @@
 # name: custom-plugin
 # about: this is a custom plugin for Discourse
-# version: 0.3
+# version: 0.4
 # authors: Pankaj Gupta
 
 register_asset "stylesheets/custom-plugin.css", :desktop
 register_asset "javascripts/discourse/templates/user/user.hbs"
 register_asset "javascripts/discourse/initializers/custom-plugin.js.es6"
+register_asset "assets/javascripts/discourse/widgets/header.js.es6"
