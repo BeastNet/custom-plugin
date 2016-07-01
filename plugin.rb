fb991ea0d3f853c617e622d78dcf563a3a8bf46a
P <prasen@rathore.com.au>
2016-07-02 02:40:09 +0930

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,4 +6,3 @@
 register_asset "stylesheets/custom-plugin.css", :desktop
 register_asset "javascripts/discourse/templates/user/user.hbs"
 register_asset "javascripts/discourse/initializers/custom-plugin.js.es6"
-register_asset "javascripts/discourse/widgets/header.js.es6"
